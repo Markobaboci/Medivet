781ee3a60b466cef72acd7b3ca5207ad7a116e20
Markobaboci <markobaboci@gmail.com>
2024-10-30 18:52:47 +0100

    added crud functioniality to the pets model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_25_175554) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_25_180522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clinics", force: :cascade do |t|
+    t.string "email"
+    t.string "phone_numer"
+    t.string "webpage"
+    t.string "address"
+    t.string "description"
+    t.string "species"
+    t.float "logitude"
+    t.float "latitude"
+    t.float "rate"
+    t.string "care_type"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clinics_on_user_id"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "species"
+    t.string "breed"
+    t.float "weight"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_25_175554) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "clinics", "users"
+  add_foreign_key "pets", "users"
 end
