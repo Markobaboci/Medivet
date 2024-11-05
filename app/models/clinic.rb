8a7ba34ad0810f56b57821d37682338bcc596d1f
joseph heron <jheronn77@gmail.com>
2024-11-05 12:58:29 +0000

    edited some files after merging

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -13,10 +13,10 @@ class Clinic < ApplicationRecord
   validates :webpage, presence: true, uniqueness: true, format: { with: URI::regexp(%w[http https]) }
   validates :address, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :species, presence: true, inclusion: { in: %w[cat dog bird rabbit exotic], message: "%{value} is not a valid species" }
-  validates :logitude, presence: true, uniqueness: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
-  validates :latitude, presence: true, uniqueness: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :species, presence: true, inclusion: { in: %w[cat dog bird rabbit exotic], message: "%{value} is not a valid species, must be either : cat, dog, bird, rabbit, or exotic" }
+  # validates :logitude, presence: true, uniqueness: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  # validates :latitude, presence: true, uniqueness: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
   validates :rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :care_type, presence: true, inclusion: { in: %w[general emergency vaccination specialty], message: "%{value} is not a valid care type" }
+  validates :care_type, presence: true, inclusion: { in: %w[general emergency vaccination specialty], message: "%{value} is not a valid care type, must be either : general, emergency, vaccination, or specialty" }
   has_many :bookings, dependent: :destroy
 end
